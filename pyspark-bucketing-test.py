369f13056982d0d6253ab6844cb4c8ee83a77a55
Alexander Hurd <hurdad@gmail.com>
2017-09-11 14:47:47 -0400

    updates

--- pyspark-bucketing-test.py
+++ pyspark-bucketing-test.py
@@ -8,7 +8,7 @@ from benchmark import benchmark
 
 logger = logging.getLogger(__name__)
 
-BUCKET_SIZE = 50000
+BUCKET_SIZE = 20000
 KEYS = 10000000
 
 
@@ -45,7 +45,6 @@ def main():
     def bucket_string_lookup(partition):
         r = redis.StrictRedis(host='ziox1.home.lan', port=6379, db=2)
         obj = r.register_script(LUA)
-        r.flushdb()
         return [
             obj(args=['hash' + ':' + str(int(hash(str(key).zfill(20)) % BUCKET_SIZE)), str(key).zfill(20), "maxkey"])
             for key in partition]
@@ -53,15 +52,16 @@ def main():
     def bucket_integer_lookup(partition):
         r = redis.StrictRedis(host='ziox1.home.lan', port=6379, db=2)
         obj = r.register_script(LUA)
-        r.flushdb()
         return [obj(args=['hash' + ':' + str(int(key % BUCKET_SIZE)), key, "maxkey"]) for key in partition]
 
+    r_driver.flushdb()
     with benchmark('bucket string'):
         rdd.mapPartitions(bucket_string_lookup).collect()
 
     logger.debug("used_memory_human : {}".format(r_driver.info()['used_memory_human']))
     logger.debug("used_memory : {}".format(r_driver.info()['used_memory']))
 
+    r_driver.flushdb()
     with benchmark('bucket integer'):
         rdd.mapPartitions(bucket_integer_lookup).collect()
 
@@ -81,12 +81,14 @@ def main():
         r.flushdb()
         return [obj(args=['hash', key, "maxkey"]) for key in partition]
 
+    r_driver.flushdb()
     with benchmark('non bucket string'):
         rdd.mapPartitions(non_bucket_string_lookup).collect()
 
     logger.debug("used_memory_human : {}".format(r_driver.info()['used_memory_human']))
     logger.debug("used_memory : {}".format(r_driver.info()['used_memory']))
 
+    r_driver.flushdb()
     with benchmark('non bucket integer'):
         rdd.mapPartitions(non_bucket_integer_lookup).collect()
 
